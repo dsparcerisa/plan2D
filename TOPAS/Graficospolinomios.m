d4fa921f3a807cf48e77ead74da8eabb0cf6a389
miguemol <58773552+miguemol@users.noreply.github.com>
2019-12-22 16:21:04 +0100

    Script que saca los graficos de los polinomios de las sigmas

---
+++ TOPAS/Graficospolinomios.m
@@ -0,0 +1,28 @@
+PFLU=[PolynomialFluence.E,PolynomialFluence.Afluence,PolynomialFluence.Bfluence,PolynomialFluence.Cfluence];
+PEDEP=[PolynomialEnergyDeposited.E,PolynomialEnergyDeposited.AEdep,PolynomialEnergyDeposited.BEdep,PolynomialEnergyDeposited.CEdep];
+NZ=2000;
+dZ=0.02;
+Zvalues = dZ*(1:NZ) - dZ/2;
+figure
+for i=1:10;
+    SfluInterp=polyval(PFLU(i,2:4),Zvalues);
+    plot(Zvalues,SfluInterp)
+    hold on
+end
+title('Fluence');
+xlabel('Sigma (cm)');
+ylabel('Z(cm)');
+legend('1MeV','2MeV','3MeV','4MeV','5MeV','6MeV','7MeV','8MeV','9MeV','10MeV');
+figure
+for i=1:10;
+    SEdepInterp=polyval(PEDEP(i,2:4),Zvalues);
+    plot(Zvalues,SEdepInterp)
+    hold on
+end
+title('Energy Deposited');
+xlabel('Sigma (cm)');
+ylabel('Z(cm)');
+legend('1MeV','2MeV','3MeV','4MeV','5MeV','6MeV','7MeV','8MeV','9MeV','10MeV');
+
+
+
