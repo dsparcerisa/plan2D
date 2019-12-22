064ee43732e60ea60f864215777a3548a440b02e
miguemol <58773552+miguemol@users.noreply.github.com>
2019-12-22 14:01:29 +0100

    Script que genera las tablas de los polinomios para cada energía. Utiliza las funciones Importallenergies y Sigmapolynomial

---
+++ TOPAS/tablepolinomia.m
@@ -0,0 +1,18 @@
+clear all
+Importallenergies
+
+PFLU=nan(10,4);
+PEDEP=nan(10,4);
+PFLU(:,1)=[1:10]';
+PEDEP(:,1)=[1:10]';
+for i=1:10;
+    [pflu,pEdep]=Sigmapolynomial(FLU(:,i),EDEP(:,i));
+    PFLU(i,(2:4))=pflu;
+    PEDEP(i,(2:4))=pEdep;
+end
+
+E=PFLU(:,1);Afluence=PFLU(:,2);Bfluence=PFLU(:,3);Cfluence=PFLU(:,4);
+PolynomialFluence=table(E,Afluence,Bfluence,Cfluence)
+
+E=PFLU(:,1);AEdep=PEDEP(:,2);BEdep=PEDEP(:,3);CEdep=PEDEP(:,4);
+PolynomialEnergyDeposited=table(E,AEdep,BEdep,CEdep)
