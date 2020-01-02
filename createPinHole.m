cc31eed492fd3ba2befdda3697abbf392d2a4ca0
Daniel Sánchez Parcerisa <dsparcerisa@ucm.es>
2020-01-02 16:27:19 +0000

    createPinHole function working

--- createPinHole.m
+++ createPinHole.m
@@ -3,5 +3,11 @@ function beamProfile = createPinHole(dxy, sizeX, sizeY, pinholeRadius)
 %   double sizeY, double pinholeRadius) 
 % Creates a CG2D object contaning a beam profile with open/closed pixels
 % simulating a pinhole collimator. Sum is NORMALIZED to 1.
+beamProfile = createEmptyCG2D(dxy, sizeX, sizeY);
+Xval = beamProfile.getAxisValues('X');
+Yval = beamProfile.getAxisValues('Y');
+distanceMatrix = sqrt(Xval.^2 + (Yval').^2);
+beamProfile.data = distanceMatrix<=pinholeRadius;
+beamProfile.data = beamProfile.data / sum(beamProfile.data(:));
 end
 
