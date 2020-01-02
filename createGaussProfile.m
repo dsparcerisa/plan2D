091a25135f159f440664a60f8f4e43dd2b94ea67
Daniel Sánchez Parcerisa <dsparcerisa@ucm.es>
2020-01-02 16:10:19 +0000

    Esqueleto de funciones que crean CG2D para un tamaño dado.

---
+++ createGaussProfile.m
@@ -0,0 +1,7 @@
+function beamProfile = createGaussProfile(dxy, sizeX, sizeY, sigmaX, sigmaY)
+% CG2D beamProfile = createPinHole(double dxy, double sizeX, 
+%   double sizeY, double sigmaX, double sigmaY) 
+% Creates a CG2D object contaning a beam profile with Gaussian shape
+% and given sigmaX / sigmaY values. Sum is NORMALIZED to 1.
+end
+
---
+++ createPinHole.m
@@ -0,0 +1,7 @@
+function beamProfile = createPinHole(dxy, sizeX, sizeY, pinholeRadius)
+% CG2D beamProfile = createPinHole(double dxy, double sizeX, 
+%   double sizeY, double pinholeRadius) 
+% Creates a CG2D object contaning a beam profile with open/closed pixels
+% simulating a pinhole collimator. Sum is NORMALIZED to 1.
+end
+
