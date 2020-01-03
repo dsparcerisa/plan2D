7b5cf752e1460383334f9537b38f66a3077c3181
Irene <isanz07@ucm.es>
2020-01-03 09:20:19 +0100

    Cambio en los comentarios

--- planScript.m
+++ planScript.m
@@ -1,11 +1,10 @@
 
 clear all
 %LECTURA 
-planTable = readtable('plan.csv'); 
-[parameters_Table,xyqMatrix, xyq_Table] = readPlan(planTable);
-%pongo como parámetro de salida xyqMatrix también porque para poder unir las dos tablas en la función writeTable (concretamente en dlmwrite), necesito el formato matriz
+planTable = readtable('plan.csv'); %readTable solo lo carga a workspace todo en formato texto
+[parameters_Table,xyqMatrix, xyq_Table] = readPlan(planTable); %lee el plan en dos tablas, ya en formato numérico
 
-%ESCRITURA, la función writePlan devuelve un fichero csv
+%ESCRITURA, la función writePlan devuelve un fichero csv con el plan
 writePlan(parameters_Table,xyqMatrix);
 
 
