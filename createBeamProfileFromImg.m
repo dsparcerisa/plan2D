98b5bf95b167cf5f9a23b73e4b3dbdae7cdcee2e
miguemol <58773552+miguemol@users.noreply.github.com>
2020-01-08 18:47:58 +0100

    Warning dx==dy

--- createBeamProfileFromImg.m
+++ createBeamProfileFromImg.m
@@ -1,7 +1,14 @@
-function beamProfile = createBeamProfileFromImg(dxy, sizeX, sizeY, imgPath)
+function beamProfile = createBeamProfileFromImg(dx,dy, sizeX, sizeY, sigmaXY,imgPath)
 % CG2D beamProfile = createBeamProfileFromImg(double dxy, double sizeX, 
 %   double sizeY, XXX imgPath) 
 % Creates a CG2D object contaning a beam profile from given data. 
 % Specific implementation to be decided. Sum is NORMALIZED to 1.
+if dx~=dy
+    warning('dx must be equal to dy');
+else
+    emptyCG2D = createEmptyCG2D(dx,dy, sizeX, sizeY);
+    data= imgaussfilt(imgPath,sigmaXY/dx); 
+    beamProfile = CartesianGrid2D(emptyCG2D.minX,emptyCG2D.maxX,emptyCG2D.minY,emptyCG2D.maxY,emptyCG2D.dx,emptyCG2D.dy,emptyCG2D.NX,emptyCG2D.NY,data);    
+end
 end
 
