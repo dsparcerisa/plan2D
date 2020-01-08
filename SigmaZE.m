355e34bdc90b9f956e7e649cfe26dda83151b7ea
Daniel Sánchez Parcerisa <dsparcerisa@ucm.es>
2020-01-08 19:06:39 +0100

    sigmaZE reducida a fluencia pero con las dos formas de hacer la interpolación. A priori parece que son equivalentes. OJO: Puede hacer que los testers dejen de funcionar, mañana lo dejamos funcionando todo.

--- SigmaZE.m
+++ SigmaZE.m
@@ -1,20 +1,41 @@
-function [Sigmaflu,SigmaEdep]=SigmaZE(PFLU,PEDEP,Z,E)
-%PFLU and PEDEP must 10x4 matrices where the first column is an array
-%containing the energies. The second, thrid and fourth coluumn must be each
-%coefficient of the sigma(Z) polynomial for each energy. 
+function [sigma1, sigma2] = SigmaZE(PFLU,Z,E)
+%PFLU and must be a 10x3 matrix where the first column is an array
+%containing the energies. The second and third columns are the
+%coefficients of the sigma(Z) polynomial for each energy.
+% Z entre [0:30]
+% E entre [1:10]
 
-%Interpolated polynomials for E
-Ainterpflu=interp1(PFLU(:,1),PFLU(:,2),E);
-Binterpflu=interp1(PFLU(:,1),PFLU(:,3),E);
-Cinterpflu=interp1(PFLU(:,1),PFLU(:,4),E);
-pinterpflu=[Ainterpflu,Binterpflu,Cinterpflu];
-
-AinterpEdep=interp1(PEDEP(:,1),PEDEP(:,2),E);
-BinterpEdep=interp1(PEDEP(:,1),PEDEP(:,3),E);
-CinterpEdep=interp1(PEDEP(:,1),PEDEP(:,4),E);
-pinterpEdep=[AinterpEdep,BinterpEdep,CinterpEdep];
+% Si la energía está directamente en la tabla
+tableIndex = find(PFLU(:,1)==E);
+if ~isempty(tableIndex)
+    fitPoly = [PFLU(tableIndex,2) PFLU(tableIndex,3) 0];
+    sigma1 = polyval(fitPoly,Z);
+    sigma2 = polyval(fitPoly,Z);
     
-%Caluclation of Sigma in the Z plane    
-Sigmaflu=polyval(pinterpflu,Z);
-SigmaEdep=polyval(pinterpEdep,Z);
+% Si la energía no está en la tabla
+else
+   
+    % Sigma1: Interpolar polinomio y luego calcular E
+    %Interpolated polynomials for E
+    Ainterpflu=interp1(PFLU(:,1),PFLU(:,2),E);
+    Binterpflu=interp1(PFLU(:,1),PFLU(:,3),E);
+    fitPoly = [Ainterpflu,Binterpflu,0];
+    
+    %Caluclation of Sigma in the Z plane
+    sigma1 = polyval(fitPoly,Z);
+    
+    % Sigma2: Calcular según E anterior y posterior e interpolar la media
+    Elow = floor(E);
+    Ehigh = ceil(E);
+    tableIndexLow = find(PFLU(:,1)==Elow);
+    fitPolyLow = [PFLU(tableIndexLow,2) PFLU(tableIndexLow,3) 0];    
+    fitPolyHigh = [PFLU(tableIndexLow+1,2) PFLU(tableIndexLow+1,3) 0];    
+    
+    sigmaLow = polyval(fitPolyLow, Z);
+    sigmaHigh = polyval(fitPolyHigh, Z);
+    
+    sigma2 = interp1([Elow Ehigh], [sigmaLow sigmaHigh], E);
+    
+end
 
+end
--- getSandE.m
+++ getSandE.m
@@ -1,4 +1,4 @@
-function [Ez, Sw_z] = getSandE(E0, z)
+function [Ez, Sw_z] = getSandE(E0, z, polyFlu)
 % [double Ez, double Sw_z] = getSandE(double E0, double z)
 % Returns Ez (MeV) and stopping power in water (MeV/cm) from initial energy
 % E0 (MeV) and distance z (cm)
