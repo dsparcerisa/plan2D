48e5442676a483e6f26f60d3e829d52040d64305
miguemol <58773552+miguemol@users.noreply.github.com>
2020-01-08 18:50:03 +0100

    Cambio

--- Tester_createGaussProfile.m
+++ Tester_createGaussProfile.m
@@ -1,8 +1,15 @@
 clear all
-dxy=0.01;
+close all
+dx=0.01;
+dy=0.01;
 sizeX=10;
 sizeY=10;
 sigmaX=5;
 sigmaY=5;
-beamProfile = createGaussProfile(dxy, sizeX, sizeY, sigmaX, sigmaY);
-imagesc(beamProfile.data);
+beamProfile = createGaussProfile(dx,dy, sizeX, sizeY, sigmaX, sigmaY);
+Xvalues = [beamProfile.minX]:[beamProfile.dx]:[beamProfile.maxX];
+Yvalues = [beamProfile.minY]:[beamProfile.dy]:[beamProfile.maxY];
+imagesc(Xvalues,Yvalues,beamProfile.data);
+title('Gaussian Profile');
+xlabel('X(cm)');
+ylabel('Y(cm)');
