1068d33bd572e2eec49085ce39f86bb83f6d7c25
miguemol <58773552+miguemol@users.noreply.github.com>
2020-01-08 18:49:10 +0100

    Cambio

--- createEmptyCG2D.m
+++ createEmptyCG2D.m
@@ -1,9 +1,9 @@
-function emptyCG2D = createEmptyCG2D(dxy, sizeX, sizeY)
+function emptyCG2D = createEmptyCG2D(dx,dy, sizeX, sizeY)
 % CG2D emptyCG2D = createEmptyCG2D(double dxy, double sizeX, double sizeY)
-NX = round(sizeX/dxy);
-NY = round(sizeY/dxy);
-maxX = (NX*dxy/2) - dxy/2;
-maxY = (NY*dxy/2) - dxy/2;
-emptyCG2D = CartesianGrid2D(-maxX,maxX,-maxY,maxY,dxy,dxy,NX,NY);
+NX = round(sizeX/dx);
+NY = round(sizeY/dy);
+maxX = (NX*dx/2) - dx/2;
+maxY = (NY*dy/2) - dy/2;
+emptyCG2D = CartesianGrid2D(-maxX,maxX,-maxY,maxY,dx,dy,NX,NY);
 end
 
