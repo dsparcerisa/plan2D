3e997e3e3939ecd572353459e60c34ad3ed941b2
Daniel Sánchez Parcerisa <dsparcerisa@ucm.es>
2020-01-08 23:06:53 +0100

    tester para convFluence

--- convFluence.m
+++ convFluence.m
@@ -3,5 +3,7 @@ function NZ = convFluence(N0, sigma)
 % Returns 2D fluence distribution as a CartesianGrid2D object, 
 % from initial distribution No (also as CG2S), convoluted
 % with sigma in cm.
+NZ = CartesianGrid2D(N0); 
+NZ.data = imgaussfilt(N0.data, sigma/N0.dx);
 end
 
