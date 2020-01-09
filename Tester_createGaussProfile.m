1371e55129992036ceff7cce55f5701c5d31c763
Daniel Sánchez Parcerisa <dsparcerisa@ucm.es>
2020-01-09 11:09:41 +0100

    createGaussProfile probado y tester funcionando

--- Tester_createGaussProfile.m
+++ Tester_createGaussProfile.m
@@ -4,12 +4,27 @@ dx=0.01;
 dy=0.01;
 sizeX=10;
 sizeY=10;
-sigmaX=5;
+sigmaX=1;
 sigmaY=5;
-beamProfile = createGaussProfile(dx,dy, sizeX, sizeY, sigmaX, sigmaY);
+beamProfile = createGaussProfile(dx, dy, sizeX, sizeY, sigmaX, sigmaY);
 Xvalues = [beamProfile.minX]:[beamProfile.dx]:[beamProfile.maxX];
 Yvalues = [beamProfile.minY]:[beamProfile.dy]:[beamProfile.maxY];
-imagesc(Xvalues,Yvalues,beamProfile.data);
+subplot(1,2,1);
+beamProfile.plotSlice
 title('Gaussian Profile');
 xlabel('X(cm)');
 ylabel('Y(cm)');
+subplot(2,2,2);
+Ysum = sum(beamProfile.data,1);
+plot(Xvalues, Ysum', 'o')
+FX = fit(Xvalues', Ysum', 'gauss1')
+sigmaX = FX.c1 / sqrt(2)
+hold on
+plot(Xvalues, FX(Xvalues));
+subplot(2,2,4);
+Xsum = sum(beamProfile.data,2);
+plot(Yvalues, Xsum, 'o')
+FY = fit(Yvalues', Xsum, 'gauss1')
+sigmaY = FY.c1 / sqrt(2)
+hold on
+plot(Yvalues, FY(Yvalues));
--- createGaussProfile.m
+++ createGaussProfile.m
@@ -3,13 +3,13 @@ function beamProfile = createGaussProfile(dx,dy, sizeX, sizeY, sigmaX, sigmaY)
 %   double sizeY, double sigmaX, double sigmaY) 
 % Creates a CG2D object contaning a beam profile with Gaussian shape
 % and given sigmaX / sigmaY values. Sum is NORMALIZED to 1.
-emptyCG2D = createEmptyCG2D(dx,dy, sizeX, sizeY);
-Xvalues = [emptyCG2D.minX]:[emptyCG2D.dx]:[emptyCG2D.maxX];
-Yvalues = [emptyCG2D.minY]:[emptyCG2D.dy]:[emptyCG2D.maxY];
+beamProfile = createEmptyCG2D(dx,dy, sizeX, sizeY);
+Xvalues = beamProfile.getAxisValues('X');
+Yvalues = beamProfile.getAxisValues('Y');
 [X,Y]=meshgrid(Xvalues,Yvalues);
 X0=(Xvalues(length(Xvalues)/2));
 Y0=(Yvalues(length(Yvalues)/2));             
-data=exp(-((X-X0).^2/(sqrt(2)*sigmaX)^2)-((Y-Y0).^2/(sqrt(2)*sigmaY)^2));
-beamProfile = CartesianGrid2D(emptyCG2D.minX,emptyCG2D.maxX,emptyCG2D.minY,emptyCG2D.maxY,emptyCG2D.dx,emptyCG2D.dy,emptyCG2D.NX,emptyCG2D.NY,data);
+beamProfile.data = exp(-((X-X0).^2/(sqrt(2)*sigmaX)^2)-((Y-Y0).^2/(sqrt(2)*sigmaY)^2));
+beamProfile.data = beamProfile.data / sum(beamProfile.data(:));
 end
 
