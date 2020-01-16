e7326021b853eff825adbc4470aeb53780534289
Daniel Sánchez Parcerisa <dsparcerisa@ucm.es>
2020-01-16 15:57:15 +0100

    Incluido fichero createGaussProfile.m

--- createGaussProfile.m
+++ createGaussProfile.m
@@ -1,7 +1,15 @@
-function beamProfile = createGaussProfile(dxy, sizeX, sizeY, sigmaX, sigmaY)
+function beamProfile = createGaussProfile(dx,dy, sizeX, sizeY, sigmaX, sigmaY)
 % CG2D beamProfile = createPinHole(double dxy, double sizeX, 
 %   double sizeY, double sigmaX, double sigmaY) 
 % Creates a CG2D object contaning a beam profile with Gaussian shape
 % and given sigmaX / sigmaY values. Sum is NORMALIZED to 1.
+beamProfile = createEmptyCG2D(dx,dy, sizeX, sizeY);
+Xvalues = beamProfile.getAxisValues('X');
+Yvalues = beamProfile.getAxisValues('Y');
+[X,Y]=meshgrid(Xvalues,Yvalues);
+X0 = (beamProfile.maxX + beamProfile.minX) / 2;
+Y0 = (beamProfile.maxY + beamProfile.minY) / 2;          
+beamProfile.data = exp(-((X-X0).^2/(sqrt(2)*sigmaX)^2)-((Y-Y0).^2/(sqrt(2)*sigmaY)^2));
+beamProfile.data = beamProfile.data / sum(beamProfile.data(:));
 end
 
