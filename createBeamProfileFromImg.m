d16c86160e39d6da3fd86393d1bd182a59815900
Daniel Sánchez Parcerisa <dsparcerisa@ucm.es>
2020-01-16 15:35:45 +0100

    Revision ficheros Miguel que nunca llegue a subir

--- createBeamProfileFromImg.m
+++ createBeamProfileFromImg.m
@@ -1,14 +1,24 @@
-function beamProfile = createBeamProfileFromImg(dx,dy, sizeX, sizeY, sigmaXY,imgPath)
-% CG2D beamProfile = createBeamProfileFromImg(double dxy, double sizeX, 
-%   double sizeY, XXX imgPath) 
-% Creates a CG2D object contaning a beam profile from given data. 
+function [beamProfile, beamProfile2] = createBeamProfileFromImg(dx, dy, sizeX, sizeY, sigmaXY, imgPath)
+% CG2D beamProfile = createBeamProfileFromImg(double dxy, double sizeX,
+%   double sizeY, XXX imgPath)
+% Creates a CG2D object contaning a beam profile from given data.
 % Specific implementation to be decided. Sum is NORMALIZED to 1.
 if dx~=dy
     warning('dx must be equal to dy');
 else
-    emptyCG2D = createEmptyCG2D(dx,dy, sizeX, sizeY);
-    data= imgaussfilt(imgPath,sigmaXY/dx); 
-    beamProfile = CartesianGrid2D(emptyCG2D.minX,emptyCG2D.maxX,emptyCG2D.minY,emptyCG2D.maxY,emptyCG2D.dx,emptyCG2D.dy,emptyCG2D.NX,emptyCG2D.NY,data);    
+    tic
+    emptyCG2D = createEmptyCG2D(dx, dy, sizeX, sizeY);
+    data = imgaussfilt(imgPath,sigmaXY/dx);
+    beamProfile = CartesianGrid2D(emptyCG2D.minX,emptyCG2D.maxX,emptyCG2D.minY,emptyCG2D.maxY,emptyCG2D.dx,emptyCG2D.dy,emptyCG2D.NX,emptyCG2D.NY,data);
+    beamProfile.data = beamProfile.data / sum(beamProfile.data(:));
+    toc
+    
+    tic
+    beamProfile2 = createEmptyCG2D(dx, dy, sizeX, sizeY);
+    gaussProfile = createGaussProfile(dx, dy, sizeX, sizeY, sigmaXY, sigmaXY);
+    beamProfile2.data = conv2(imgPath, gaussProfile.data, 'same');
+    beamProfile2.data = beamProfile2.data / sum(beamProfile2.data(:));
+    toc
 end
 end
 
--- createGaussProfile.m
+++ createGaussProfile.m
@@ -7,8 +7,8 @@ beamProfile = createEmptyCG2D(dx,dy, sizeX, sizeY);
 Xvalues = beamProfile.getAxisValues('X');
 Yvalues = beamProfile.getAxisValues('Y');
 [X,Y]=meshgrid(Xvalues,Yvalues);
-X0=(Xvalues(length(Xvalues)/2));
-Y0=(Yvalues(length(Yvalues)/2));             
+X0 = (beamProfile.maxX + beamProfile.minX) / 2;
+Y0 = (beamProfile.maxY + beamProfile.minY) / 2;          
 beamProfile.data = exp(-((X-X0).^2/(sqrt(2)*sigmaX)^2)-((Y-Y0).^2/(sqrt(2)*sigmaY)^2));
 beamProfile.data = beamProfile.data / sum(beamProfile.data(:));
 end
