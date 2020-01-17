dcd9f46cb5e3c4b8a316d57371b3e7c8521deb67
Daniel Sánchez Parcerisa <dsparcerisa@ucm.es>
2020-01-17 11:34:24 +0100

    Read/Write plan from DesarrolloIrene

--- readPlan.m
+++ readPlan.m
@@ -1,19 +1,42 @@
-function plan = readPlan(fileName, app)
-% struct plan = readPlan(string fileName)
-% Reads a plan into a structure from specified fileName
+function [parameters_Table, xyqMatrix, xyq_Table] = readPlan(planTable)
+%El plan se va a leer en dos partes debido a sus dimensiones
+%A continuación se lee la primera fila del plan
+parametersTable = planTable(1,1:5);
 
-app.logLine(['Opening file ' fileName]);
+energy = parametersTable(:,1);
+numericEnergy = table2array(energy);
+E = str2double(numericEnergy);
 
-% Needs to fill in the following data
-% plan.fileName = 
-% plan.name =
-% plan.energy = 
-% plan.Z = 
-% plan.nominalIntensity = 
-% plan.pepperPot = 
-% plan.Nspots =
-% plan.XposVector =
-% plan.YposVector =
-% plan.QVector =
+z = parametersTable(:,2);
+numericZ = table2array(z);
+Z = str2double(numericZ);
+
+intensity = parametersTable(:,3);
+numericIntensity = table2array(intensity);
+I = str2double(numericIntensity);
+
+filterCode = parametersTable(:,4);
+codFiltro = table2array(filterCode);
+
+numberSpots = parametersTable(:,5);
+numSpots = table2array(numberSpots);
+
+parameters_Table = table(E,Z,I,codFiltro,numSpots);
+parameters_Table.Properties.VariableNames{'E'} = 'E_MeV';
+parameters_Table.Properties.VariableNames{'Z'} = 'Z_cm';
+parameters_Table.Properties.VariableNames{'I'} = 'I_nA';
+
+%Se leen el resto de filas del plan
+xyqTable = planTable(3:end,1:3); 
+numericXYQtable = table2array(xyqTable);
+
+x = str2double(numericXYQtable(:,1));
+y = str2double(numericXYQtable(:,2));
+q = str2double(numericXYQtable(:,3));
+xyq_Table = table(x,y,q);
+xyq_Table.Properties.VariableNames{'x'} = 'X_cm';
+xyq_Table.Properties.VariableNames{'y'} = 'Y_cm';
+xyq_Table.Properties.VariableNames{'q'} = 'Q_pC';
+xyqMatrix = [x,y,q];
 end
 
--- writePlan.m
+++ writePlan.m
@@ -1,5 +1,9 @@
-function writePlan(plan, outFileName)
-% void writePlan(struct plan, string outFileName)
-% saves plan intro filename outFileName
+function writePlan(parameters_Table,xyqMatrix)
+%Se crea un archivo csv en el que se escribe parameters_Table y,posteriormente, se añaden a este archivo los valores de X, Y y Q.
+writetable (parameters_Table, 'outputPlan.csv'); 
+header = {'X','Y','Q'};
+dlmwrite('outputPlan.csv',header,'-append');
+dlmwrite('outputPlan.csv',xyqMatrix,'-append');
+
 end
 
