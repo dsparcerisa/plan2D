4b3962b354a678cfe35862929e641d9b90280c5b
Daniel Sánchez Parcerisa <dsparcerisa@ucm.es>
2020-01-17 11:25:38 +0100

    BugFix en convFluence para tener en cuenta el tamaño del filtro.

--- convFluence.m
+++ convFluence.m
@@ -4,7 +4,8 @@ function NZ = convFluence(N0, sigma)
 % from initial distribution No (also as CG2S), convoluted
 % with sigma in cm.
 NZ = CartesianGrid2D(N0); 
-NZ.data = imgaussfilt(N0.data, sigma/N0.dx);
+filterSize = round(max(N0.NX, N0.NY) / 2)*2 + 1;
+NZ.data = imgaussfilt(N0.data, sigma/N0.dx, 'FilterSize',filterSize);
 NZ.data = NZ.data / sum(NZ.data(:)); % Renormalizar
 end
 
