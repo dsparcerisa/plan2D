5b8c783bfe4615052a7cec30e3cfcbfe848c9fef
Daniel Sánchez Parcerisa <dsparcerisa@ucm.es>
2020-01-21 12:21:24 +0100

    Minimos cambios en getDoseFromPlan y añadir pepperpots a la GUI

--- getDoseFromPlan.m
+++ getDoseFromPlan.m
@@ -1,11 +1,11 @@
-function dose = getDoseFromPlan(doseCanvas, plan, E0, z, dz, targetTh, targetSPR, N0)
+function dose = getDoseFromPlan(doseCanvas, plan, dz, targetTh, targetSPR, N0)
 % CartesianGrid2D N0 = getNfromPlan(struct plan, CG2D profile0)
 % Returns fluence map at Z=0 from struct plan and profile from single beam
 
 pC2pNumber = 1e7/1.602176634;
 doseCanvas.data(:) = 0;
 limits = [doseCanvas.minX doseCanvas.maxX doseCanvas.minY doseCanvas.maxY];
-doseKernel = getDoseMap(E0, z, dz, pC2pNumber, targetTh, targetSPR, N0);
+doseKernel = getDoseMap(plan.E, plan.Z, dz, pC2pNumber, targetTh, targetSPR, N0);
 
 for i=1:plan.numSpots
     Di = doseKernel.copy;
