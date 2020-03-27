e51156c67e42631375ce0a0f70d7b0f1dd0a95c4
Daniel Sánchez Parcerisa <dsparcerisa@ucm.es>
2020-03-27 17:18:56 +0100

    limpieza directorio

--- getNfromPlan.m
+++
@@ -1,7 +0,0 @@
-function dose = getDosefromPlan(plan, E0, z, dz, targetTh, targetSPR, N0)
-% CartesianGrid2D N0 = getNfromPlan(struct plan, CG2D profile0)
-% Returns fluence map at Z=0 from struct plan and profile from single beam
-
-
-end
-
