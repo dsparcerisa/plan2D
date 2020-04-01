b1af99bfad644cf91f7d578dbb6ad7128052644e
Daniel Sánchez Parcerisa <dsparcerisa@ucm.es>
2020-04-01 15:26:52 +0200

    cambios con isoutlier

--- Logs/FLASH_regresion.m
+++ Logs/FLASH_regresion.m
@@ -1,6 +1,9 @@
+%% Carga
 clear all
 load('tablas_logs_FLASH')
 TABLA = who;
+
+%% Calculo de los tiempos y plot
 shots = [];
 Time = [];
 for i = 1:length(TABLA)
@@ -8,26 +11,39 @@ for i = 1:length(TABLA)
      shots = [shots;tbl.FLASH_shots];
      Time = [Time;tbl.Cierre_s-tbl.Apertura_s];
 end
-
-Mean = nan(50,2);
-for S = 1:50;  %Numero de shots
-    ss = find(shots == S);
-    Mean(S,2) = mean(Time(ss));
-    Mean(S,1) = S;
-end
-
-Mean2 = Mean(:,2);
-Flash_shots = find( Mean2>0 );
-Flash_time = Mean2(Flash_shots);
-lin = polyfit(Flash_shots,Flash_time,1)
+plot(shots, Time, '+');
+grid on
+% %%
+% Mean = nan(50,2);
+% for S = 1:50  %Numero de shots
+%     ss = find(shots == S);
+%     Mean(S,2) = mean(Time(ss));
+%     Mean(S,1) = S;
+% end
+% 
+% Mean2 = Mean(:,2);
+% Flash_shots = find( Mean2>0 );
+% Flash_time = Mean2(Flash_shots);
+%% Fit
+subplot(2,1,1)
+lin = polyfit(shots, Time,1)
 F = 0:0.01:50;
 FF = polyval(lin,F);
 
-plot(Flash_shots,Flash_time,'+',F,FF)
+plot(shots,Time,'+',F,FF)
 xlabel('Number of Flash Shots')
 ylabel('Time (s)')
     
-        
-        
+%% Residue
+
+subplot(2,1,2);
+residue = (Time - polyval(lin, shots)).^2; 
+stdResidue = std(residue);
+hold off;
+plot(shots, residue, 'o');
+outlierMask = residue>2*stdResidue;
+hold on
+plot(shots(outlierMask), residue(outlierMask), 'rx')
+plot([1 50], [stdResidue stdResidue], 'k:');
         
     
--- Logs/NON_FLASH_regresion.m
+++ Logs/NON_FLASH_regresion.m
@@ -10,6 +10,7 @@ for i = 1:length(TABLA)
 end
 
 validPoints = ~isoutlier(Time,'percentiles', [1 99]);
+%validPoints = 1:numel(Time);
 validShots = shots(validPoints);
 validTime = Time(validPoints);
 
@@ -17,9 +18,24 @@ lin = polyfit(validShots,validTime,1)
 F = 0:0.01:50;
 FF = polyval(lin,F);
 
+subplot(2,1,1);
 plot(validShots,validTime,'+',F,FF)
 xlabel('Number of Flash Shots')
 ylabel('Time (s)')
 grid on
 
+%% Residue
+
+subplot(2,1,2);
+residue = (validTime - polyval(lin, validShots)).^2; 
+stdResidue = std(residue);
+hold off;
+plot(validShots, residue, 'o');
+outlierMask = residue>stdResidue;
+hold on
+plot(validShots(outlierMask), residue(outlierMask), 'rx')
+plot([1 50], [stdResidue stdResidue], 'k:');
+subplot(2,1,1);
+hold on
+plot(validShots(outlierMask), validTime(outlierMask), 'rx')
 %% Ahora habría que usar de nuevo isoutlier (o un threshold en el residuo) para seleccionar los que se alejan de la curva
