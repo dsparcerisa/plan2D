554cacb9d45c27174dd4203b8fb22069c6690989
miguemol <58773552+miguemol@users.noreply.github.com>
2020-04-08 13:14:51 +0200

    Cambios en los scripts de regresion para estudio de residuos por irradiacion

--- Logs/FLASH_regresion.m
+++ Logs/FLASH_regresion.m
@@ -1,5 +1,6 @@
 %% Carga
 clear all
+close all
 load('tablas_logs_FLASH')
 TABLA = who;
 
@@ -13,20 +14,10 @@ for i = 1:length(TABLA)
 end
 plot(shots, Time, '+');
 grid on
-% %%
-% Mean = nan(50,2);
-% for S = 1:50  %Numero de shots
-%     ss = find(shots == S);
-%     Mean(S,2) = mean(Time(ss));
-%     Mean(S,1) = S;
-% end
-% 
-% Mean2 = Mean(:,2);
-% Flash_shots = find( Mean2>0 );
-% Flash_time = Mean2(Flash_shots);
+
 %% Fit
 subplot(2,1,1)
-lin = polyfit(shots, Time,1)
+lin = polyfit(shots, Time,1);
 F = 0:0.01:50;
 FF = polyval(lin,F);
 
@@ -38,12 +29,29 @@ ylabel('Time (s)')
 
 subplot(2,1,2);
 residue = (Time - polyval(lin, shots)).^2; 
-stdResidue = std(residue);
+stdResidue_FLASH = std(residue);
 hold off;
 plot(shots, residue, 'o');
-outlierMask = residue>2*stdResidue;
+outlierMask = residue>2*stdResidue_FLASH;
 hold on
 plot(shots(outlierMask), residue(outlierMask), 'rx')
-plot([1 50], [stdResidue stdResidue], 'k:');
-        
-    
+plot([1 50], [stdResidue_FLASH stdResidue_FLASH], 'k:');
+
+%%
+for i = 1:length(TABLA);
+    tbl = evalin('base',TABLA{i});
+    shots_i = [tbl.FLASH_shots];
+    Time = [tbl.Cierre_s-tbl.Apertura_s];
+    residue = (Time - polyval(lin, shots_i)).^2;
+    outlier = residue>stdResidue_FLASH;
+    R = [tbl.Numero_exposiciones,Time,residue,outlier];
+    Failed_Exposure = tbl.Numero_exposiciones(outlier);
+    Percentage_Deviation = abs(1-residue(outlier)./stdResidue_FLASH).*100; %Mirar
+    F = table(Failed_Exposure,Percentage_Deviation);
+    eval([['res_',TABLA{i}],'=R;']);
+    eval([['failed_exposures_',TABLA{i}],'=F;']);
+end
+clear tbl shots_i Time residue outlier R Failed_Exposure Percentage_Deviation F TABLA
+save('residues_FLASH.mat','res_*','stdResidue_FLASH');
+save('failed_exposures_FLASH.mat','failed_e*');
+   
--- Logs/NON_FLASH_regresion.m
+++ Logs/NON_FLASH_regresion.m
@@ -1,14 +1,21 @@
 clear all
+close all
 load('tablas_logs_NON_FLASH')
 TABLA = who;
 shots = [];
 Time = [];
+%AX = [];
+%Time_AX = [];
 for i = 1:length(TABLA)
      tbl = evalin('base',TABLA{i});
      shots = [shots;tbl.Tiempo_apertura_teorico_s];
      Time = [Time;tbl.Cierre_s-tbl.Apertura_s];
+     %AX = [AX;sum(abs(tbl.XYZ_final_cm-tbl.XYZ_inicial_cm),2)];
+     %Time_AX = [Time_AX;tbl.Tiempo_posicion_final_s-tbl.Tiempo_posicion_inicial_s]
+     
 end
-
+figure
+%plot(AX,Time_AX)
 validPoints = ~isoutlier(Time,'percentiles', [1 99]);
 %validPoints = 1:numel(Time);
 validShots = shots(validPoints);
@@ -28,14 +35,34 @@ grid on
 
 subplot(2,1,2);
 residue = (validTime - polyval(lin, validShots)).^2; 
-stdResidue = std(residue);
+stdResidue_NON_FLASH = std(residue);
 hold off;
 plot(validShots, residue, 'o');
-outlierMask = residue>stdResidue;
+outlierMask = residue>stdResidue_NON_FLASH;
 hold on
 plot(validShots(outlierMask), residue(outlierMask), 'rx')
-plot([1 50], [stdResidue stdResidue], 'k:');
+plot([1 50], [stdResidue_NON_FLASH stdResidue_NON_FLASH], 'k:');
 subplot(2,1,1);
 hold on
 plot(validShots(outlierMask), validTime(outlierMask), 'rx')
 %% Ahora habría que usar de nuevo isoutlier (o un threshold en el residuo) para seleccionar los que se alejan de la curva
+
+for i = 1:length(TABLA);
+    tbl = evalin('base',TABLA{i});
+    shots_i = [tbl.Tiempo_apertura_teorico_s];
+    Time = [tbl.Cierre_s-tbl.Apertura_s];
+    residue = (Time - polyval(lin, shots_i)).^2;
+    outlier = residue>stdResidue_NON_FLASH;
+    R = [tbl.Numero_exposiciones,Time,residue,outlier];
+    Failed_Exposure = tbl.Numero_exposiciones(outlier);
+    Percentage_Deviation = abs(1-residue(outlier)./stdResidue_NON_FLASH).*100;
+    F = table(Failed_Exposure,Percentage_Deviation);
+    eval([['res_',TABLA{i}],'=R;']);
+    eval([['failed_exposures_',TABLA{i}],'=F;']);
+end
+save('residues_NON_FLASH.mat','res_*','stdResidue_NON_FLASH');
+save('failed_exposures_NON_FLASH.mat','failed_e*');
+
+
+
+
