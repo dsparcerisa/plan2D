b4a10d99a109b30624d675e017cd2f7f2e3383cd
MIGUEL MOLINA HERNANDEZ <miguemol@ucm.es>
2020-04-12 19:55:37 +0200

    Cálculo de residuos para el tiempo de movimiento del portamuestros

--- Logs/FLASH_regresion.m
+++ Logs/FLASH_regresion.m
@@ -54,4 +54,44 @@ end
 clear tbl shots_i Time residue outlier R Failed_Exposure Percentage_Deviation F TABLA
 save('residues_FLASH.mat','res_*','stdResidue_FLASH');
 save('failed_exposures_FLASH.mat','failed_e*');
+%% Distances-Time
+AX = [];
+Time_AX = [];
+for i = 1:length(TABLA)
+     tbl = evalin('base',TABLA{i});
+     AX1 = tbl.XYZ_inicial_cm;
+     [n,l] = size(AX1);
+     AX1 = sum(abs(AX1(2:n,:)-AX1(1:n-1,:)),2);
+     AX = [AX;AX1];
+     Time_AX1 = [tbl.Tiempo_posicion_final_s-tbl.Tiempo_posicion_inicial_s];
+     Time_AX = [Time_AX;Time_AX1(2:length(Time_AX1))];
+     
+end
+figure
+validPoints = ~isoutlier(Time_AX,'percentiles', [1 99]);
+validAX = AX(validPoints);
+validTime_AX = Time_AX(validPoints);
+
+lin = polyfit(validAX,validTime_AX,1)
+F = 0:0.01:15;
+FF = polyval(lin,F);
+
+subplot(2,1,1);
+plot(validAX,validTime_AX,'+',F,FF)
+xlabel('\DeltaX')
+ylabel('Time (s)')
+grid on
+%Residue
+subplot(2,1,2);
+residue = (validTime_AX - polyval(lin, validAX)).^2; 
+stdResidue_AX_FLASH = std(residue);
+hold off;
+plot(validAX, residue, 'o');
+outlierMask = residue>stdResidue_AX_FLASH;
+hold on
+plot(validAX(outlierMask), residue(outlierMask), 'rx')
+plot([1 15], [stdResidue_AX_FLASH stdResidue_AX_FLASH], 'k:');
+subplot(2,1,1);
+hold on
+plot(validAX(outlierMask), validTime_AX(outlierMask), 'rx')
    
--- Logs/NON_FLASH_regresion.m
+++ Logs/NON_FLASH_regresion.m
@@ -1,23 +1,19 @@
+%% load data
 clear all
 close all
 load('tablas_logs_NON_FLASH')
 TABLA = who;
+
+%% Time-shots
 shots = [];
 Time = [];
-%AX = [];
-%Time_AX = [];
 for i = 1:length(TABLA)
      tbl = evalin('base',TABLA{i});
      shots = [shots;tbl.Tiempo_apertura_teorico_s];
-     Time = [Time;tbl.Cierre_s-tbl.Apertura_s];
-     %AX = [AX;sum(abs(tbl.XYZ_final_cm-tbl.XYZ_inicial_cm),2)];
-     %Time_AX = [Time_AX;tbl.Tiempo_posicion_final_s-tbl.Tiempo_posicion_inicial_s]
-     
+     Time = [Time;tbl.Cierre_s-tbl.Apertura_s];     
 end
 figure
-%plot(AX,Time_AX)
 validPoints = ~isoutlier(Time,'percentiles', [1 99]);
-%validPoints = 1:numel(Time);
 validShots = shots(validPoints);
 validTime = Time(validPoints);
 
@@ -62,6 +58,46 @@ for i = 1:length(TABLA);
 end
 save('residues_NON_FLASH.mat','res_*','stdResidue_NON_FLASH');
 save('failed_exposures_NON_FLASH.mat','failed_e*');
+%% Distances-Time
+AX = [];
+Time_AX = [];
+for i = 1:length(TABLA)
+     tbl = evalin('base',TABLA{i});
+     AX1 = tbl.XYZ_inicial_cm;
+     [n,l] = size(AX1);
+     AX1 = sum(abs(AX1(2:n,:)-AX1(1:n-1,:)),2);
+     AX = [AX;AX1];
+     Time_AX1 = [tbl.Tiempo_posicion_final_s-tbl.Tiempo_posicion_inicial_s];
+     Time_AX = [Time_AX;Time_AX1(2:length(Time_AX1))];
+     
+end
+figure
+validPoints = ~isoutlier(Time_AX,'percentiles', [1 99]);
+validAX = AX(validPoints);
+validTime_AX = Time_AX(validPoints);
+
+lin = polyfit(validAX,validTime_AX,1)
+F = 0:0.01:15;
+FF = polyval(lin,F);
+
+subplot(2,1,1);
+plot(validAX,validTime_AX,'+',F,FF)
+xlabel('\DeltaX')
+ylabel('Time (s)')
+grid on
+%Residue
+subplot(2,1,2);
+residue = (validTime_AX - polyval(lin, validAX)).^2; 
+stdResidue_AX_NON_FLASH = std(residue);
+hold off;
+plot(validAX, residue, 'o');
+outlierMask = residue>stdResidue_AX_NON_FLASH;
+hold on
+plot(validAX(outlierMask), residue(outlierMask), 'rx')
+plot([1 15], [stdResidue_AX_NON_FLASH stdResidue_AX_NON_FLASH], 'k:');
+subplot(2,1,1);
+hold on
+plot(validAX(outlierMask), validTime_AX(outlierMask), 'rx')
 
 
 
