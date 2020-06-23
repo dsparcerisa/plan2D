fdbdbcb917fff2ec8891dffbf54de1eafaa50c2b
MIGUEL MOLINA HERNANDEZ <miguemol@ucm.es>
2020-06-23 21:52:13 +0200

    Gamma 3

---
+++ Gamma3.m
@@ -0,0 +1,27 @@
+function [G] = Gamma3(DcProfile,DmProfile)
+% Generates the Gamma matrix for qualitative evaluations of dose
+% distributions. Source: Low, Daniel A., et al. "A technique for the quantitative evaluation of dose distributions." Medical physics 25.5 (1998): 656-661.
+% The bin Profile must be in mm and the dose must be relative
+%%
+[Rcx,Rcy] = getRadius(DcProfile);
+[Rmx,Rmy] = getRadius(DmProfile);
+%%
+Dc = DcProfile.data;
+Dm = DmProfile.data;
+ADm = 3; % Percentage
+Adm = 3; % mm
+G = nan(DmProfile.NY,DmProfile.NX);
+%%
+for i = 1:numel(G);
+    rmi = [Rmx(i) Rmy(i)];
+    Dmi = Dm(i);
+    T = nan(DcProfile.NY,DcProfile.NX);
+    for j = 1:numel(T);
+        rcj = [Rcx(j) Rcy(j)];
+        Dcj = Dc(j);
+        r = sqrt(sum((rcj-rmi).^2));
+        d = Dcj-Dmi;
+        T(j) = sqrt((r/Adm)^2+(d/ADm)^2);
+    end
+    G(i) = min(min(T));
+end
---
+++ Tester_Gamma3.m
@@ -0,0 +1,22 @@
+clear all
+close all
+
+
+Dc1Profile = createEmptyCG2D(1,1, 3, 3);
+Dc1Profile.data = [1 3 1;3 5 3;1 3 1];
+Dm1Profile = createEmptyCG2D(1,1, 3, 3);
+Dm1Profile.data = [300 3 1;5 6 4;1 2 40];
+
+[G1] = Gamma3(Dc1Profile,Dm1Profile)
+
+%% Comparativa imgaussfilt-gaussiana
+
+beamProfile = createGaussProfile(0.2,0.2, 20, 20, 5, 5);
+[imgaussfiltProfile, gaussProfile] = createBeamProfileFromImg(0.2, 0.2, 20,20, 5, beamProfile.data);
+[G2] = Gamma3(imgaussfiltProfile, gaussProfile)
+
+%% Comparativa diferente bineado
+
+beamProfile1 = createGaussProfile(0.2,0.2, 20, 20, 5, 5);
+beamProfile2 = createGaussProfile(0.3,0.3, 20, 20, 5, 5);
+[G3] = Gamma3(beamProfile1, beamProfile2)
