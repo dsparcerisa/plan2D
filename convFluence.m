d70541e1fe45c8db79ed8553b2d12ef6a8f6dc68
MIGUEL MOLINA HERNANDEZ <miguemol@ucm.es>
2020-06-25 09:17:21 +0200

    no message

--- convFluence.m
+++ convFluence.m
@@ -3,5 +3,9 @@ function NZ = convFluence(N0, sigma)
 % Returns 2D fluence distribution as a CartesianGrid2D object, 
 % from initial distribution No (also as CG2S), convoluted
 % with sigma in cm.
+NZ = CartesianGrid2D(N0); 
+filterSize = round(max(N0.NX, N0.NY) / 2)*2 + 1;
+NZ.data = imgaussfilt(N0.data, sigma/N0.dx, 'FilterSize',filterSize);
+NZ.data = NZ.data / sum(NZ.data(:)); % Renormalizar
 end
 
--- getDoseMap.m
+++ getDoseMap.m
@@ -1,5 +1,27 @@
-function doseMap = getDoseMap(E0,z,dz,Nprot,targetTh,targetDens,targetSPR,N0)
+function doseMap = getDoseMap(E0,z,dz,Nprot,targetTh,targetSPR,N0)
 % CG2D doseMap = getDoseMap(double E0, double z, double dz, double Nprot, 
 %  double targetTh, double targetDens, double targetSPR, CG2D N0)
+ 
+[sigma, Sw_z] = getSandE(E0, z, dz);
+ 
+if exist('sigmaPoly')
+    sigma = polyval(sigmaPoly, z) / 10;
+    NZ = createGaussProfile(N0.dx, N0.dy, N0.dx*N0.NX, N0.dy*N0.NY, sigma, sigma);
+else
+    NZ = convFluence(N0, sigma);
+end
+ 
+MeV2J = 1.602e-13;
+ 
+E_Mev = Nprot*NZ.data*(Sw_z*targetSPR)*targetTh;
+E_J = E_Mev * MeV2J;
+ 
+m_1voxel_g = (N0.dx * N0.dy * targetTh);
+m_1voxel_kg = m_1voxel_g / 1000;
+ 
+D = E_J ./ m_1voxel_kg;
+ 
+doseMap = CartesianGrid2D(N0);
+doseMap.data = D;
 end
 
